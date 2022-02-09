3014e86ba8e577a5972d3dcc0e611eb9007e49cf
simatheone <samone07@mail.ru>
2022-02-09 19:51:32 +0300

    Add jwt token and basic configurations, add viewset for posts

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import PostViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,9 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from api.serializers import PostSerializer
+from posts.models import Comment, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
