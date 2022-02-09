f4e95500d683adb80dee21c15c99e690c455c660
simatheone <samone07@mail.ru>
2022-02-09 23:04:40 +0300

    Дописал вьюсет для Комментариев

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
 
 from api.serializers import (
     CommentSerializer, FollowSerializer,
@@ -40,13 +41,22 @@ class CommentViewSet(viewsets.ModelViewSet):
     posts/<post_id>/comments/ и posts/<post_id>/comments/<comment_id>/
     """
     serializer_class = CommentSerializer
-    # permission_classes = 
+    permission_classes = (IsAuthorOrReadOnly,) 
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
         queryset = Comment.objects.filter(post_id=post_id)
         return queryset
 
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_update(self, serializer):
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        instance.delete()
 
 class FollowViewSet(viewsets.ModelViewSet):
     """Вьюсет для обработки CRUD запросов follow/... ."""
