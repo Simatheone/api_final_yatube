6ad1a4d6d25d68dcf1272a99800eeede435bb76b
simatheone <samone07@mail.ru>
2022-02-09 22:50:54 +0300

    Добавил в админ панель модели Post, Group, Comment, Follow

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Comment, Follow, Group, Post
+from yatube_api.settings import EMPTY_VALUE_DISPLAY
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Класс для отображения постов в админ панели."""
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+        'image'
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = EMPTY_VALUE_DISPLAY
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    """Класс для отоброжения групп в админ панели."""
+    list_display = ('title',)
+    empty_value_display = EMPTY_VALUE_DISPLAY
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Класс для отоброжения комментариев в админ панели."""
+    list_display = (
+        'post',
+        'text',
+        'author',
+        'created'
+    )
+    search_fields = ('text',)
+    list_filter = ('created',)
+    empty_value_display = EMPTY_VALUE_DISPLAY
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    """Класс для отображения подписок в админ панели."""
+    list_display = ('user', 'author')
+    search_fields = ('user',)
