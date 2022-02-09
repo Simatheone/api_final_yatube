7e0da08537e1f6eaa04e5018faa1bbf6ef25bca7
simatheone <samone07@mail.ru>
2022-02-09 20:39:11 +0300

    Расписал сериализаторы для моделей Comment, Group, Post

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,22 +2,42 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """Cериализатор для модели Post."""
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'text', 'group', 'author', 'pub_date', 'image')
+        read_only_fields = ('id', 'pub_date', 'image')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group."""
+    
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+        read_only_fields = ('id', 'title', 'slug', 'description')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    """Сериализатор модели Comment."""
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('id', 'post', 'created')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализотор модели Follow."""
+    
+    class Meta:
+        model = Follow
+        # Проверить и подправить
+        fields = '__all__'
