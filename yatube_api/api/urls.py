e78bc5747a7b826d9be25f2f05f87753bb7be41c
simatheone <samone07@mail.ru>
2022-02-09 20:24:21 +0300

    Добавил эндпоинты для будущих вьюсетов. Необходимо скорректировать некоторые ендпоинты через regex

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import PostViewSet
+from api.views import (
+    CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+    )
 
 router_v1 = DefaultRouter()
 router_v1.register(r'posts', PostViewSet, basename='posts')
+router_v1.register(r'groups', GroupViewSet, basename='groups')
+router_v1.register(r'comments', CommentViewSet, basename='comments')
+router_v1.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls))
