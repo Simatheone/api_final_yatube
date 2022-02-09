b1a3ecd22e5d0dbc962bed357d15293397362d79
simatheone <samone07@mail.ru>
2022-02-09 22:52:26 +0300

    Убрал ordering в модели пост. Это мешало правильное работе пагинатора

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    """Кастомный пермишшен для проверки авторства."""
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.method in SAFE_METHODS
